615bb5917803cb9221e603af7776de88e90c7888
yanna <yannafaith97@gmail.com>
2022-12-29 01:09:23 -0500

    fixes styled component composition bug

--- src/tasks/views/Home.jsx
+++ src/tasks/views/Home.jsx
@@ -5,43 +5,43 @@ import {PageCtn} from "../styles/containers";
 import {MainHeader, SubHeader} from "../styles/headers";
 import {StartBtn} from "../styles/buttons";
 
-const HomeWrapper = styled(PageCtn)`{}`
-const HomeH1 = styled(MainHeader)`{
+const HomeWrapper = styled(PageCtn)``;
+const HomeH1 = styled(MainHeader)`
   margin-bottom: 30px;
-}`;
-const HomeH2 = styled(SubHeader)`{
+`;
+const HomeH2 = styled(SubHeader)`
   margin-top: 100px;
   margin-bottom: 30px;
-}`;
-const FeatureCtn = styled.div`{
+`;
+const FeatureCtn = styled.div`
   border: 1px solid black;
   width: 400px; 
   height: 200px;
   display: flex;
   justify-content: center;
   padding: 50px;
-}`;
-const FeatureHeader = styled.h2`{
+`;
+const FeatureHeader = styled.h2`
   font-size: 2rem;
-}`;
-const MainCopy = styled.p`{
+`;
+const MainCopy = styled.p`
     font-size: 1.5rem;
-}`;
-const Features = styled.div`{
+`;
+const Features = styled.div`
     width: 90%;
     display: flex;
     flex-direction: row;
     justify-content: space-around;
-}`;
-const BtnsCtn = styled.div`{
+`;
+const BtnsCtn = styled.div`
   width: 70%;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-}`;
-const HomeBtn = styled(StartBtn)`{
+`;
+const HomeBtn = styled(StartBtn)`
     margin-top: 100px;
-}`;
+`;
 
 const Home = () => {
     return (
